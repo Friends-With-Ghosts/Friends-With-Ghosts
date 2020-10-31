dfdead8a10b4b419d1e41b465a931bdddc015999
jeronartest <artestjeron@gmail.com>
2020-10-30 23:33:47 -0700

    animations imported

---
+++ Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    CharacterController controller;
+    public GameObject player;
+    public Animator animator;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = player.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", controller.velocity.magnitude);
+
+    }
+}
--- Assets/Scripts/ThirdPersonCharacter.cs
+++ Assets/Scripts/ThirdPersonCharacter.cs
@@ -7,28 +7,62 @@ public class ThirdPersonCharacter : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float currentSpeed = 0f;
+
+    public float speed;
+    public float gravity;
     
     public float turnSmoothTime = 0.1f;
-    public float turnSmoothVelocity;
+    private float turnSmoothVelocity;
+
     
+
+    public float speedSmoothTime = 0.3f;
+    private float speedSmoothVelocity;
+    public bool isGrounded = false;
+
+    public GameObject gfx;
+    Animator animator ;
+
+    
+
+    private void Start() {
+        animator = gfx.GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+     
+    }
+
+    void Move(){
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 gravityVector = Vector3.zero;
+
+        if(!controller.isGrounded)
+        {
+            gravityVector.y -= gravity;
+        }  
 
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             
+            float targetSpeed = speed * direction.magnitude;
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-
+            controller.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
+            controller.Move(gravityVector * Time.deltaTime);
         }
+
+        animator.SetFloat("Speed", direction.magnitude, speedSmoothTime, Time.deltaTime );
+
     }
 }
